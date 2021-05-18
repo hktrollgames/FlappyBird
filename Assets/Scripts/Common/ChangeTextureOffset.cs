b261ee7320023d43121856aa52bc660349f6a896
hktrollgames <hktrollgames@gmail.com>
2021-05-18 17:05:04 +0900

    수정했어요

--- Assets/Scripts/Common/ChangeTextureOffset.cs
+++ Assets/Scripts/Common/ChangeTextureOffset.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +11,7 @@ public class ChangeTextureOffset : MonoBehaviour
         material = GetComponent<Renderer>().material;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         var offset = material.mainTextureOffset;
