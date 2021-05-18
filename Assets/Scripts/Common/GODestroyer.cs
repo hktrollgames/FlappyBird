a0d391b589cd01c1c860c3573b07aeed642fe9a6
hktrollgames <hktrollgames@gmail.com>
2021-05-18 17:12:33 +0900

    이건 5월 20일에 강의할 내용

--- Assets/Scripts/Common/GODestroyer.cs
+++ Assets/Scripts/Common/GODestroyer.cs
@@ -1,14 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GODestroyer : MonoBehaviour
 {
     public float minX = int.MinValue;
-
+    // 이건 아직 작업중이라서 올려선 안되는거
     void Update()
     {
         if (transform.position.x < minX)
             Destroy(gameObject);
     }
+
+
+    // 이건 5월 20일에 강의할 내용
 }
